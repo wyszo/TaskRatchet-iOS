cda4d600ac835fb8f52247e9797c379baad40a98
ThomasW <thomas.wyszomirski.dev@gmail.com>
2023-09-10 08:37:29 +0100

    AddTaskView broken down into subviews

--- TaskRatchet-iOS/TaskRatchet-iOS/AddTask/AddTaskView.swift
+++ TaskRatchet-iOS/TaskRatchet-iOS/AddTask/AddTaskView.swift
@@ -22,75 +22,8 @@ struct AddTaskView: View {
                 )
                     .textFieldStyle(.roundedBorder)
 
-                // Stakes row
-                // TODO: extract to a separate view
-                HStack {
-                    Text("Stakes:")
-                    TextField("Stakes (dollars):",
-                              text: viewStore.binding(
-                                get: { $0.newTask.stakes.dollarsRoundedDownString },
-                                send: { .ui(.stakesChanged(cents: (Int($0) ?? 0) * 100)) }
-                              )
-                    )
-                        .frame(maxWidth: 50)
-                        .multilineTextAlignment(.trailing)
-                        .textFieldStyle(.roundedBorder)
-                    Text("$")
-                    Spacer()
-                    
-                    Group {
-                        Button("(-1)") {
-                            viewStore.send(.ui(.stakesIncreased(byCents: -100)))
-                        }
-                        Button("(+1)") {
-                            viewStore.send(.ui(.stakesIncreased(byCents: 100)))
-                        }
-                    }
-                        .buttonStyle(.bordered)
-                }
-                
-                // Predefined stakes row
-                // TODO: extract to a separate view
-                HStack{
-                    Spacer()
-                    Group {
-                        Button("$2") {
-                            viewStore.send(.ui(.stakesChanged(cents: 200)))
-                        }
-                        Button("$5") {
-                            viewStore.send(.ui(.stakesChanged(cents: 500)))
-                        }
-                        Button("$10") {
-                            viewStore.send(.ui(.stakesChanged(cents: 1000)))
-                        }
-                    }
-                    .buttonStyle(.bordered)
-                }
-                
-
-                // Due date row
-                // TODO: extract this to a separate view
-                VStack {
-                    DatePicker("Due date",
-                               selection: viewStore.binding(
-                                get: \.newTask.dueDate,
-                                send: { .ui(.dueDateChanged($0))}
-                               ))
-                    
-                    // Common task deadlines: 1h, 2h, 4h from now,
-                    // day, week, month from now
-                    HStack {
-                        DueDateButton(caption: "In 1h", dueDate: .now.dateByAddingHours(1), viewStore: viewStore)
-                        DueDateButton(caption: "In 2h", dueDate: .now.dateByAddingHours(2), viewStore: viewStore)
-                        DueDateButton(caption: "In 4h", dueDate: .now.dateByAddingHours(4), viewStore: viewStore)
-
-                        Spacer()
-                        
-                        DueDateButton(caption: "In a day", dueDate: .now.dateByAddingDay, viewStore: viewStore)
-                        DueDateButton(caption: "In a week", dueDate: .now.dateByAddingWeek, viewStore: viewStore)
-                        DueDateButton(caption: "In a month", dueDate: .now.dateByAddingMonth, viewStore: viewStore)
-                    }
-                }
+                StakesRows(viewStore: viewStore)
+                DueDateRows(viewStore: viewStore)
                 
                 Spacer()
                     .frame(minHeight: 8, maxHeight: 8)
@@ -148,5 +81,97 @@ private struct DueDateButton: View {
         }
         .buttonStyle(.bordered)
         .buttonBorderShape(.roundedRectangle)
+    }
+}
+
+struct StakesRows: View {
+    let viewStore: ViewStore<AddTask.State, AddTask.Action>
+    
+    var body: some View {
+        StakesRow(viewStore: viewStore)
+        PredefinedStakesRow(viewStore: viewStore)
+    }
+}
+
+struct StakesRow: View {
+    
+    let viewStore: ViewStore<AddTask.State, AddTask.Action>
+    
+    var body: some View {
+        HStack {
+            Text("Stakes:")
+            TextField("Stakes (dollars):",
+                      text: viewStore.binding(
+                        get: { $0.newTask.stakes.dollarsRoundedDownString },
+                        send: { .ui(.stakesChanged(cents: (Int($0) ?? 0) * 100)) }
+                      )
+            )
+                .frame(maxWidth: 50)
+                .multilineTextAlignment(.trailing)
+                .textFieldStyle(.roundedBorder)
+            Text("$")
+            Spacer()
+            
+            Group {
+                Button("(-1)") {
+                    viewStore.send(.ui(.stakesIncreased(byCents: -100)))
+                }
+                Button("(+1)") {
+                    viewStore.send(.ui(.stakesIncreased(byCents: 100)))
+                }
+            }
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.roundedRectangle)
+        }
+    }
+}
+
+struct PredefinedStakesRow: View {
+    let viewStore: ViewStore<AddTask.State, AddTask.Action>
+
+    var body: some View {
+        HStack{
+            Spacer()
+            Group {
+                Button("$2") {
+                    viewStore.send(.ui(.stakesChanged(cents: 200)))
+                }
+                Button("$5") {
+                    viewStore.send(.ui(.stakesChanged(cents: 500)))
+                }
+                Button("$10") {
+                    viewStore.send(.ui(.stakesChanged(cents: 1000)))
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+struct DueDateRows: View {
+    let viewStore: ViewStore<AddTask.State, AddTask.Action>
+
+    var body: some View {
+        VStack {
+            DatePicker("Due date",
+                       selection: viewStore.binding(
+                        get: \.newTask.dueDate,
+                        send: { .ui(.dueDateChanged($0))}
+                       ))
+            
+            // Common task deadlines: 1h, 2h, 4h from now,
+            // day, week, month from now
+            HStack {
+                DueDateButton(caption: "In 1h", dueDate: .now.dateByAddingHours(1), viewStore: viewStore)
+                DueDateButton(caption: "In 2h", dueDate: .now.dateByAddingHours(2), viewStore: viewStore)
+                DueDateButton(caption: "In 4h", dueDate: .now.dateByAddingHours(4), viewStore: viewStore)
+                
+                Spacer()
+                
+                DueDateButton(caption: "In a day", dueDate: .now.dateByAddingDay, viewStore: viewStore)
+                DueDateButton(caption: "In a week", dueDate: .now.dateByAddingWeek, viewStore: viewStore)
+                DueDateButton(caption: "In a month", dueDate: .now.dateByAddingMonth, viewStore: viewStore)
+            }
+        }
     }
 }
